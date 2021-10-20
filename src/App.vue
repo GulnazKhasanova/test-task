839e3df9f63cb8733e7c728e36bd3487ff8044bb
GulnazKhasanova <viteewato@mail.ru>
2021-10-20 14:52:05 +0400

    start

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-view>
+    <Filterpage></Filterpage>
+
+  </router-view>
+
+</template>
+
+<script>
+
+import Filterpage from "@cf/Filterpage";
+
+
+export default {
+  name: 'App',
+  components: {Filterpage},
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+}
+</style>
